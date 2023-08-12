19bbaceade9d1a8abbd44cb1dff660849ad09f24
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2023-08-12 14:48:56 +0530

    temp fix for polyfills

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,27 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, searchForWorkspaceRoot } from 'vite';
 
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-// only needed for npm run dev as it uses esbuild rather than rollup
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+// // only needed for npm run dev as it uses esbuild rather than rollup
+// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+// import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+// import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
-import path from 'path'
+import path from 'path';
 
 export default defineConfig({
 	plugins: [
-    sveltekit(),
-    nodePolyfills({
-      globals: {
-        global: true,
-        Buffer: 'dev' 
-      },
-      protocolImports: true
-    })
-  ],
-  // esbuild: {
-  //   plugins: [NodeGlobalsPolyfillPlugin({buffer:true}), NodeModulesPolyfillPlugin()]
-  // },
+		sveltekit(),
+		nodePolyfills({
+			// globals: {
+			// 	global: true,
+			// 	Buffer: 'dev'
+			// },
+			protocolImports: true
+		})
+	],
+	// esbuild: {
+	//   plugins: [NodeGlobalsPolyfillPlugin({buffer:true}), NodeModulesPolyfillPlugin()]
+	// },
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
@@ -31,8 +32,8 @@ export default defineConfig({
 	// for resolve, optimizeDeps, build.rollupOptions
 	resolve: {
 		alias: {
-      $assets: path.resolve('./static'),
-      $api: path.resolve('/src/routes/api'),
+			$assets: path.resolve('./static'),
+			$api: path.resolve('/src/routes/api'),
 			// Errors like,
 			//  RollupError: "Buffer" is not exported by "__vite-browser-external"
 			// can be resolved by adding the appropriate polyfill alias here
@@ -40,19 +41,29 @@ export default defineConfig({
 			// buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6' OK but this package is sort of abandoned
 			// path: 'rollup-plugin-polyfill-node/dist/polyfills.js',
 			// buffer: 'rollup-plugin-polyfill-node/dist/polyfills.js'
+			stream: 'stream-browserify',
+			crypto: 'crypto-browserify',
+			assert: 'assert'
 		}
 	},
 	optimizeDeps: {
-  	include: ['ethers', '@web3auth/modal'],
-    esbuildOptions: {
-      plugins: [NodeGlobalsPolyfillPlugin({buffer:true}), NodeModulesPolyfillPlugin()]
-    }
+		include: [],
+		esbuildOptions: {
+			define: {
+				global: 'globalThis'
+			},
+			plugins: []
+		}
 	},
 	server: {
 		fs: {
-			allow: [
-				searchForWorkspaceRoot(process.cwd())
-			]
+			allow: [searchForWorkspaceRoot(process.cwd())]
+		}
+	},
+	build: {
+		target: 'es2022',
+		rollupOptions: {
+			plugins: []
 		}
 	}
 });
